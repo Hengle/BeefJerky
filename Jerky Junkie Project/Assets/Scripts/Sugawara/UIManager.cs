971fad2ef598bf0ec289f5364fb06552cb8aa8db
Ysugashi <sugashi0713@google.com>
2017-12-12 11:31:39 +0900

    文字点滅処理、およびその処理のテスト用のシーンを追加 ポーズ処理をビルド用シーンに実装

---
+++ Jerky Junkie Project/Assets/Scripts/Sugawara/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	[SerializeField] private Slider timeSlider = null;
+	[SerializeField] private GameObject pauseWindow = null;
+	[SerializeField] private bool pause = false;
+	[SerializeField] private Button pauseButton = null;
+	private Image pauseButtonImage = null;
+
+	[SerializeField] private Sprite[] pauseButtonSprite = null;
+
+	private float maxT = 100f;
+	private float t;
+
+	private void Start()
+	{
+		t = maxT;
+		pauseWindow.SetActive(false);
+		pauseButtonImage = pauseButton.GetComponent<Image>();
+	}
+
+	void Update () {
+		t -= Time.deltaTime * 5.0f;
+		TimeUpdate(t);
+	}
+
+	public void TimeUpdate(float time)
+	{
+		timeSlider.value = t/maxT;
+	}
+
+	public void Pause()
+	{
+		pause = !pause;
+		pauseWindow.SetActive(pause);
+		if (pause)
+		{
+			pauseButtonImage.sprite = pauseButtonSprite[1];
+			Time.timeScale = 0.0f;
+		}
+		else
+		{
+			pauseButtonImage.sprite = pauseButtonSprite[0];
+			Time.timeScale = 1.0f;
+		}
+	}
+
+	public void ExitButton()
+	{
+		//タイトルシーンへ遷移
+	}
+}
