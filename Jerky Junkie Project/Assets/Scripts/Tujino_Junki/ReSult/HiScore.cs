105a3bfe65429731faaa8415863db955b62dcdc5
富澤勇太 <gontyanzoo@gmail.com>
2017-12-14 05:11:20 +0900

    HiScoreクラスをシングルトンに更新

--- Jerky Junkie Project/Assets/Scripts/Tujino_Junki/ReSult/HiScore.cs
+++ Jerky Junkie Project/Assets/Scripts/Tujino_Junki/ReSult/HiScore.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HiScore : MonoBehaviour {
+public class HiScore : SingletonMonoBehaviour<HiScore> {
         
     public Text scoreText;// スコアを表示する
     
