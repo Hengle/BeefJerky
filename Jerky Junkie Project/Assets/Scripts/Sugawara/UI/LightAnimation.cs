157ab6ddfc8ca4fc4dafd1b75213b72165905fa0
Ysugashi <sugashi0713@google.com>
2017-12-14 10:56:09 +0900

    提灯アニメーション改良版です

--- Jerky Junkie Project/Assets/Scripts/Sugawara/UI/LightAnimation.cs
+++ Jerky Junkie Project/Assets/Scripts/Sugawara/UI/LightAnimation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LightAnimation : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class LightAnimation : MonoBehaviour {
 	private RectTransform rectTransform;
 	[SerializeField] float countTime = 0.0f;
 	[SerializeField] float magnification = 1.2f; //倍率
+
+	[SerializeField] Image[] lightImage = null;
 
 	private float startscale;
 
@@ -26,7 +29,6 @@ public class LightAnimation : MonoBehaviour {
 
 	private void Update()
 	{
-		Debug.Log(rectTransform.localScale.x);
 		countTime += Time.deltaTime;
 		if(startTime == 0)
 		{
@@ -53,6 +55,11 @@ public class LightAnimation : MonoBehaviour {
 			objScale.x = Mathf.Clamp(objScale.x,startscale,startscale * magnification);
 			objScale.y = Mathf.Clamp(objScale.y, startscale, startscale * magnification);
 			objScale.z = Mathf.Clamp(objScale.z, startscale, startscale * magnification);
+			rectTransform.localScale = objScale;
+			Debug.Log(objScale.x.ToString() + "  :  " + (startscale * magnification).ToString() + " = " + (objScale.x / (startscale*magnification)));
+			lightImage[0].color = new Color(lightImage[0].color.r, lightImage[0].color.g, lightImage[0].color.b,((objScale.x/(startscale*magnification)-0.2f) * 0.4f));
+			lightImage[1].color = new Color(lightImage[1].color.r, lightImage[1].color.g, lightImage[1].color.b, ((objScale.x / (startscale * magnification) - 0.2f) * 0.4f));
+
 			if (rectTransform.localScale.x >= startscale * magnification)
 			{
 				stats = -1;
