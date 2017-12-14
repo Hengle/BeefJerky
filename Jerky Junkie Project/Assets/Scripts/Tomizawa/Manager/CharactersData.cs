73c6d73c65993da69a2163ca634b90217a720fd6
富澤勇太 <gontyanzoo@gmail.com>
2017-12-14 20:53:41 +0900

    ジャーキー爆弾の作成

---
+++ Jerky Junkie Project/Assets/Scripts/Tomizawa/Manager/CharactersData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/CharactersData")]
+public class CharactersData : ScriptableObject {
+    public List<Character> CharacetrPrefab;
+    public Character Ozisan;
+    public List<Character> Jakis;
+}
